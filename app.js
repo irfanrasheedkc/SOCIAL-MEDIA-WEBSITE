18a91950f45de344e0f28dc533d7063a2c42c9a8
irfan rasheed <irfanrasheed273@gmail.com>
2023-02-17 15:35:02 +0530

    CORS

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(logger('dev'));
 app.use(express.json());
