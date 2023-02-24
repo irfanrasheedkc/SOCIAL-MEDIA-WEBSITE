db2a88b315de014f500283816d04f8d5baa7fef2
irfan rasheed <irfanrasheed273@gmail.com>
2023-02-24 19:34:56 +0530

    Login and signup api

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,23 +1,80 @@
-var db = require('../config/connection')
+const db = require('../app')
+// const users = db.users
+// const images = db.images
 const collection = require('../config/collection')
 const bcrypt = require('bcrypt');
 
+var objectId = require('mongodb').ObjectId
+
 module.exports = {
-    doSignup:async (userData) => {
-        console.log(userData)
-        data = {
-            name: userData.name,
-            email: userData.email,
-            password: await bcrypt.hash(userData.password, 10)
+    doSignup: async (userData, imageData) => {
+        userData.password = await bcrypt.hash(userData.password, 10);
+        const result = await db.users.create(userData)
+        if (imageData) {
+            imageData = imageData.image
+            const newImage = new db.images({
+                name: imageData.name,
+                data: imageData.data,
+                mimetype: imageData.mimetype,
+                size: imageData.size,
+                uid: result._id
+            });
+
+            newImage.save((err, savedImage) => {
+                if (err) {
+                    console.log('Error saving image:', err);
+                } else {
+                    console.log('Image saved successfully:', savedImage);
+                }
+            });
         }
-        return new Promise((resolve, reject) => {
-            db.get().collection(collection.USER_COLLECTION).insertOne(data).then((err, res) => {
-                if (err)
-                    console.log(err)
-                else
-                    console.log(res)
-                resolve()
-            })
-        })
-    }
+        return result;
+    },
+    doLogin: async (userData) => {
+        async function authenticateUser(username, password) {
+            let user = await db.users.findOne({ id: username });
+            if (!user) {
+                return null; // user not found
+            }
+
+            const isPasswordMatch = await bcrypt.compare(password, user.password);
+            if (!isPasswordMatch) {
+                return null; // password doesn't match
+            }
+            return user;
+        }
+        let user = await authenticateUser(userData.id, userData.password);
+        if (user) {
+            // Find the user by their id and retrieve only the 'name' field
+            const x = user._id
+            async function getImage(x) {
+                try {
+                    const image = await new Promise((resolve, reject) => {
+                        db.images.findOne({ uid: x }, 'data', function (err, image) {
+                            if (err) {
+                                console.log(err);
+                                reject(err);
+                            } else {
+                                console.log('Got the image');
+                                resolve(image);
+                            }
+                        });
+                    });
+                    return image;
+                } catch (err) {
+                    console.log(err);
+                    return null;
+                }
+            }
+
+            image = await getImage(x);
+            user.image = image;
+            return user;
+        } else {
+            return null;
+        }
+
+
+
+    },
 }
--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,36 @@
 var express = require('express');
 var router = express.Router();
 const userHelpers = require('../helpers/user-helpers.js');
+const users = require('../app')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+
   data = {
-    "api_status":"active"
+    "api_status": "active"
   };
   res.json(data);
 });
 
-router.post('/signup', function (req, res) {
-  userHelpers.doSignup(req.body).then((response) => {
-    res.json({"Signup":"Success"});
+router.post('/signup',  function (req, res) {
+  userHelpers.doSignup(req.body , req.files).then((response) => {
+    console.log(response)
+    res.json({"signup":true});
   })
 })
+
+router.post('/login', function (req, res) {
+  userHelpers.doLogin(req.body).then((response) => {
+    let result = {...response};
+    // let result  = [response].map(user=>user.toObject({gutter:true}));
+    result.login = true;
+    console.log(result);
+    if(response)
+      res.json(result);
+    else
+      res.json({"login":false})
+  })
+})
+
 
 module.exports = router;
